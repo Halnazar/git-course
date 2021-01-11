3affe75cec378dc7a90800fbdafca5502adf2151
Halnazar <halnazarg@gmail.com>
2021-01-11 18:42:10 +0300

    Third commit

---
+++ git-course/Source1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	cout << "Hello World" << endl;
+	cout << "Halnazar" << endl;
+
+	system("pause");
+	return 0;
+}
